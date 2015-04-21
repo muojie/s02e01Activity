97ea3787c90900c72f66ab8219250fafbf9f5ff9
gujixin <htc.guo@gmai.com>
2015-04-21 14:22:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/s02e01activity/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.s02e01activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
