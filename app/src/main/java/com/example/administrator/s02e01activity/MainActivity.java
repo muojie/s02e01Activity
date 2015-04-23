9c0878fc1df2d1545766093ea3fb363f20ed8f00
gujixin <htc.guo@gmai.com>
2015-04-23 13:11:35 +0800

    some change

--- app/src/main/java/com/example/administrator/s02e01activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/s02e01activity/MainActivity.java
@@ -64,6 +64,9 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent();
+            //setClass函数的第一个参数是一个Context对象
+            //Context是一个类，Activity是Context类的子类，也就是说，所有的Activity对象，都可以向上转型为Context对象
+            //setClass函数的第二个参数是一个Class对象，在当前场景下，应该传入需要被启动的Activity类的class对象
             intent.setClass(MainActivity.this, SecondActivity.class);
             startActivity(intent);
         }
