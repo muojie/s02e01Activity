9d148c9e872cc60a586695b01cf930890cbff764
gujixin <htc.guo@gmai.com>
2015-09-25 18:17:55 +0800

    build jni with Application.xml

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,9 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -std=c99
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
-
 LOCAL_MODULE    :=  DB_TESTER
 LOCAL_SRC_FILES :=  test.cpp
 
--- app/src/main/jni/test.c
+++ app/src/main/jni/test.c
@@ -5,5 +5,37 @@
 
 JNIEXPORT jstring JNICALL Java_com_example_administrator_s02e01activity_dbtester_DBTesterActivity_getNativeMessage
         (JNIEnv *env, jobject thisObj) {
-    return (*env)->NewStringUTF(env, "Hello from native code!");
+
+#if defined(__arm__)
+    #if defined(__ARM_ARCH_7A__)
+      #if defined(__ARM_NEON__)
+        #if defined(__ARM_PCS_VFP)
+          #define ABI "armeabi-v7a/NEON (hard-float)"
+        #else
+          #define ABI "armeabi-v7a/NEON"
+        #endif
+      #else
+        #if defined(__ARM_PCS_VFP)
+          #define ABI "armeabi-v7a (hard-float)"
+        #else
+          #define ABI "armeabi-v7a"
+        #endif
+      #endif
+    #else
+     #define ABI "armeabi"
+    #endif
+#elif defined(__i386__)
+    #define ABI "x86"
+#elif defined(__x86_64__)
+    #define ABI "x86_64"
+#elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
+    #define ABI "mips64"
+#elif defined(__mips__)
+    #define ABI "mips"
+#elif defined(__aarch64__)
+#define ABI "arm64-v8a"
+#else
+    #define ABI "unknown"
+#endif
+    return (*env)->NewStringUTF(env, "Hello from native code!  Compiled with ABI " ABI ".");
 }
