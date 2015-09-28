902bd1a08f0c2687d575849f7f01ab28bbcc07e1
gujixin <htc.guo@gmai.com>
2015-09-28 14:12:14 +0800

    resolved database operate error on fuse sdcard if build with armeabi-v7
    refer to:
    
    https://code.google.com/p/android/issues/detail?id=79994

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,1 +1,2 @@
-APP_ABI := armeabi-v7a
+// APP_ABI := armeabi-v7a    // if this run on 64bit os, there will be broken when operated database on external sdcard(fuse)
+APP_ABI := armeabi-v7a arm64-v8a
