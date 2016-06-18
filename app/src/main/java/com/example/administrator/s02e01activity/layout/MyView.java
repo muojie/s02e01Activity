dcb4a0cb7c5a5d461f8869afbe875baf591472ca
lenovo <guojie563@163.com>
2016-06-18 18:33:48 +0800

    View onMeasure onLayout onDraw

---
+++ app/src/main/java/com/example/administrator/s02e01activity/layout/MyView.java
@@ -0,0 +1,31 @@
+package com.example.administrator.s02e01activity.layout;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lenovo on 2016/6/18.
+ */
+public class MyView extends View {
+
+    private Paint mPaint;
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setTextSize(20);
+        String text = "Hello View";
+        canvas.drawText(text, 0, getHeight() / 2, mPaint);
+    }
+}
